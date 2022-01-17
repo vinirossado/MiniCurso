103c5af037bb87ea63cf724e8a83ebf40e721a30
Vinicius Rossado <vinirossado@gmail.com>
2022-01-17 13:05:44 -0300

    Removendo a parte do front e migrando pra um repo proprio

--- Crud/FrontEnd/src/app/enums/notification-color.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum NotificationColorEnum {
-    normal = 1,
-    warning = 2,
-    danger = 3
-  }
-  
--- Crud/FrontEnd/src/app/models/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './menu.model';
-export * from './notification-snack.model';
-export * from './veiculos.model';
